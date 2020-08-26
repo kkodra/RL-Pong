7b1a8dd052ef3f41f63649c43aeb5d5ffdf18ea2
kkodra <ksk2035@icloud.com>
2020-08-26 00:02:41 -0400

    Conditional to specify algorithm

--- main.py
+++ main.py
@@ -8,3 +8,16 @@ Q : Q-Learning
 import pong_Q
 import pong_MC
 
+# Select algorithm: 'MC' or 'Q'
+algo = 'MC'
+
+if algo == 'MC':
+    pong_MC
+elif algo == 'Q':
+    pong_Q
+else:
+    raise Exception('Option not available.')
+    
+
+        
+
