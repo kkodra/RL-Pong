c47e86a72ef69b5e6451107ffb6a9b6cfe65241d
kkodra <ksk2035@icloud.com>
2020-08-26 00:49:31 -0400

    Fix bugs

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python
 """
 Main script to run RL-Pong
 User can choose between two algorithms: MC and Q
@@ -5,19 +6,14 @@ MC: Monte Carlo method
 Q : Q-Learning
 """
 
-import pong_Q
-import pong_MC
-
 # Select algorithm: 'MC' or 'Q'
 algo = 'MC'
 
 if algo == 'MC':
-    pong_MC
+    import pong_MC
 elif algo == 'Q':
-    pong_Q
+    import pong_Q
 else:
     raise Exception('Option not available.')
-    
-
         
 
